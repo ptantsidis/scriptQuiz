7d2fc22c213a1baee42b856c53523fe14065b896
Philip Tantsidis <ptantsidis@gmail.com>
2021-09-22 23:23:55 -0400

    Just in time

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,7 @@ let wins = 0;
 let timer;
 let questionNumber = 0;
 let i = 0;
+let h = 0;
 
 let questions = [{
     question: "JavaScript is ______ Side scripting language.",
@@ -26,15 +27,25 @@ let questions = [{
     question: " ______ tag is an extension to HTML that can enclose any number of JavaScript statements.",
     multiChoice: ["<SCRIPT>", "<BODY>", "<HEAD>", "<TITLE>"],
     correctAnswer: "0"
+},
+{
+    question: "What should appear at the very end of your JavaScript? The <script LANGUAGE=”JavaScript”>tag",
+    multiChoice: ["The </script>", "The <script>", "The END statement", "All of the above."],
+    correctAnswer: "0"
+},
+{
+    question: "What is the correct syntax for referring to an external script called ” abc.js”?",
+    multiChoice: ["<script href=” abc.js”>", " <script name=” abc.js”>", "<script src=” abc.js”>", "None of the above"],
+    correctAnswer: "2"
+
 }]
 
-// let questionKey = Math.floor((Math.random() * questions.length) + 1);
-// no sure why I set this up
+
 function startGame() {
     initApp();
     startTime();
     displayQuestion();
-   }
+}
 
 function initApp() {
     losses = 0;
@@ -43,6 +54,7 @@ function initApp() {
     questionNumber = 0
 }
 function startTime() {
+    console.log(event.target.value)
     startButton.disabled = true;
     timeRemain = 15;
     timer = setInterval(function () {
@@ -55,38 +67,29 @@ function startTime() {
     }, 1000);
 }
 
-function displayQuestion() { 
-    console.log("displayQuestion");
-    if (questionNumber === 0) {
+function displayQuestion() {
+   if (questionNumber != questions.length) {
+        multiChoiceBlock.innerHTML = "";
         questionBlock.textContent = questions[questionNumber].question;
         for (i = 0; i < questions[questionNumber].multiChoice.length; i++) {
-            console.log(questionNumber);
-            item = document.createElement('li');          
+            item = document.createElement('li');
             item.textContent = questions[questionNumber].multiChoice[i];
-            item.value=i;
+            item.value = i;
             item.addEventListener("click", markAnswer);
             multiChoiceBlock.appendChild(item);
-            console.log(questions[questionNumber].correctAnswer[0]);
         }
     } else {
-        questionBlock.textContent = questions[questionNumber].question;
-        for (i = 0; i < questions[questionNumber].multiChoice.length; i++) {
-            item.textContent = questions[questionNumber].multiChoice[i];
-            item.value= i;
-            item.addEventListener("click", markAnswer());
-            multiChoiceBlock.appendChild(item);
-            item = document.replaceChildren('li', 'li');
-            console.log(questions[questionNumber].correctAnswer);
-        }
+        winGame();
     }
 }
 
+
 function markAnswer(event) {
-console.log("markanswer")
+    console.log(questions[questionNumber].correctAnswer)
     if (timeRemain === 0) {
         clearInterval(timer);
         lostGame();
-        }
+    }
 
     if (!event.target.value == questions[questionNumber].correctAnswer) {
         lostGame();
@@ -98,29 +101,33 @@ console.log("markanswer")
 function lostGame() {
     console.log("lostgame")
     losses++;
-    timeRemain  = timeRemain - 5;
+    timeRemain = timeRemain - 5;
     questionNumber++
-    displayQuestion();
+    if ((timeRemain > 0) && (questionNumber != questions.length)) {
+        displayQuestion();
+    } else {
+        return;
+    }
 }
 
 function winGame() {
     console.log("wingame")
-    wins++; 
-    timeRemain  = timeRemain + 5;
+    wins++;
+    timeRemain = timeRemain + 5;
     questionNumber++;
     displayQuestion();
 }
 function logGame() {
-// // write to local storage
-//     let gameLog = {
-//         // name: name.value,
-//         losses: losses.value,
-//         wins: wins.value,
-//     }
+    // // write to local storage
+    //     let gameLog = {
+    //         // name: name.value,
+    //         losses: losses.value,
+    //         wins: wins.value,
+    //     }
 }
 
 localStorage.setItem("logGame", JSON.stringify(logGame));
-  
+
 
 startButton.addEventListener("click", startGame);
 
