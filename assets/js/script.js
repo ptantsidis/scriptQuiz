e8f187a8d747e79faac918cad1108b46100e500e
Philip Tantsidis <ptantsidis@gmail.com>
2021-09-23 19:56:02 -0400

    further along

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
 let timerEl = document.querySelector(".time-remain");
 let winsEl = document.querySelector(".wins");
-let lossesEl = document.querySelector(".losees");
-let overEl = document.querySelector(".Over");
+let lossesEl = document.querySelector(".losses");
+let overEl = document.querySelector(".over");
 let startButton = document.querySelector(".start-button");
 let logButton = document.querySelector(".log-button");
 let questionBlock = document.querySelector("#question-list");
 let multiChoiceBlock = document.querySelector("#multi-choice-list");
 let item = document.createElement('li');
+let userSignup = document.querySelector("#initials");
+let gamesWon = document.querySelector("#gamesWon");
+let gameslost = document.querySelector("#gamesLost");
 
 let timeRemain = 15;
 let losses = 0;
@@ -48,24 +51,27 @@ function startGame() {
     initApp();
     startTime();
     displayQuestion();
-
 }
 
 function initApp() {
     losses = 0;
     wins = 0;
     startButton.disabled = false;
-    questionNumber = 0
+    logButton.disabled = false;
+    questionNumber = 0;
+    winsEl.textContent = 0;
+    lossesEl.textContent = 0;
 }
 function startTime() {
     startButton.disabled = true;
+    logButton.disabled = true;
     timeRemain = 15;
     timer = setInterval(function () {
         timeRemain--;
         timerEl.textContent = timeRemain;
         if (timeRemain === 0) {
             clearInterval(timer);
-            markAnswer();
+            gameOver();
         }
     }, 1000);
 }
@@ -87,8 +93,7 @@ function displayQuestion() {
 }
 
 function markAnswer(event) {
-    console.log(questions[questionNumber].correctAnswer)
-
+    event.preventDefault();
     if (event.target.value == questions[questionNumber].correctAnswer) {
         winGame();
     } else {
@@ -97,12 +102,10 @@ function markAnswer(event) {
 }
 
 function lostGame() {
-
-    console.log("lostgame");
-    losses++ ;
+    losses++;
     lossesEl.textContent = losses;
     questionNumber++
-    if ((timeRemain > 0) && (questionNumber != questions.length)) {
+    if ((timeRemain > 5) && (questionNumber != questions.length)) {
         timeRemain = timeRemain - 5;
         displayQuestion();
     } else {
@@ -114,7 +117,7 @@ function winGame() {
     console.log("wingame")
     wins++;
     winsEl.textContent = wins;
-    
+
     questionNumber++;
     if ((timeRemain > 0) && (questionNumber != questions.length)) {
         timeRemain = timeRemain + 5;
@@ -126,19 +129,24 @@ function winGame() {
 
 function gameOver() {
     clearInterval(timer);
-    over.textContent = "Game Over"
-    return;
-}
-function logGame() {
-    confirm("Log Results to local storage?")
-    // // write to local storage
-    //     let gameLog = {
-    //         // name: name.value,
-    //         losses: losses.value,
-    //         wins: wins.value,
-    //     }
+    timeRemain = 0;
+    overEl.textContent = ("Game Over");
+    logButton.disabled = false;
+    startButton.disabled = false;
+    questionBlock.textContent = ("A Valiant Attempt");
+    multiChoiceBlock.innerHTML = "";
 }
 
-localStorage.setItem("logGame", JSON.stringify(logGame));
 startButton.addEventListener("click", startGame);
-logButton.addEventListener("click", logGame);
+logButton.addEventListener("click", function (event) {
+    event.preventDefault();
+    let initials = document.querySelector("#initials").value;
+    logButton.disabled = true;
+
+    if (initials === "") {
+        displayMessage("error", "Initials cannot be blank");
+        localStorage.setItem("initials", initials);
+        localStorage.setItem("gamesWon", gameswon);
+        localStorage.setItem("gamelost", gameslost);
+    }
+});
